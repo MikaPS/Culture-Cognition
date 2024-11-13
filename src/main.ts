2eb131017898e6f263157910e63c88967adfb3a3
MikaPS <mikaps7@gmail.com>
2024-11-12 21:30:46 -0800

    some instructions + citations

--- src/main.ts
+++ src/main.ts
@@ -75,7 +75,7 @@ function investmentCreator(): Investment {
     time_investment[0];
   const randPayout =
     Math.floor(
-      Math.random() * (payout_investment[1] - payout_investment[0] + 1),
+      Math.random() * (payout_investment[1] - payout_investment[0] + 1)
     ) + payout_investment[0];
   const randCost =
     Math.floor(Math.random() * (cost_investment[1] - cost_investment[0] + 1)) +
@@ -91,7 +91,7 @@ function investmentCreator(): Investment {
     id: currentPinID,
   };
   console.log(
-    "current pin: " + currentPinID + " investment id: " + investment.id,
+    "current pin: " + currentPinID + " investment id: " + investment.id
   );
 
   return investment;
@@ -136,6 +136,16 @@ function uiTextDialouge(heightOffset: number) {
   // env_img.style.left = `${700}px`;
   // env_img.style.top = `${0 + heightOffset}px`;
   // dialougeApp.append(env_img);
+
+  const instructions = document.createElement("span");
+  instructions.style.display = "inline-block";
+  instructions.style.textAlign = "left";
+  instructions.innerText =
+    "You're going to read about two companies.\nEach has a different descriptions and statistics, and you can only invest in one.\nChoose carefully between the two, or do nothing.";
+  instructions.style.position = "absolute";
+  instructions.style.top = `${height * 0.01}px`;
+  instructions.style.left = `${width * 0.01}px`;
+  dialougeApp.appendChild(instructions);
 
   const rectangle = document.createElement("span");
   rectangle.className = "rectangle"; // Defined in the CSS
@@ -290,6 +300,8 @@ function newDay() {
   paper.onload = () => {
     // Create the text element
     const text = document.createElement("span");
+    text.className = "text-box";
+    text.style.setProperty("--width", `${width * 0.25}px`);
     text.innerText = `Day: ${day}\nMoney: ${money}\nAny payments:\n\t${newPayments}`;
     text.style.position = "absolute";
     text.style.top = "150px";
@@ -303,31 +315,31 @@ function newDay() {
   // const bg = document.createElement("span");
   // bg.className = "new_day_rect"; // Defined in the CSS
   // const text = document.createElement("span");
-  const relativePos = container.getBoundingClientRect();
+  // const relativePos = container.getBoundingClientRect();
   const closedInvestments: Investment[] = checkIfInvestmentSucceeded();
 
   let newPayments = "";
   closedInvestments.forEach((investment) => {
-    newPayments += `${investment.name} got you ${investment.payout}`;
+    newPayments += `${investment.name} got you ${investment.payout}\n`;
   });
   openInvestments.forEach((investment) => {
     newPayments += `You need to wait ${investment.time} days to see the payout\nof ${investment.symbol}${investment.name}${investment.symbol}\n`;
   });
-  dayApp.append(container);
 
   const yes = document.createElement("span");
-  yes.textContent = `👍`;
+  yes.textContent = `Move to the next day: 👍`;
   yes.style.position = "absolute";
-  yes.style.top = `${relativePos.top + 70}px`;
-  yes.style.left = `${relativePos.left + 5}px`;
+  yes.style.top = `${height * 0.7}px`;
+  yes.style.left = `${width * 0.22}px`;
   yes.addEventListener("click", () => {
     dayApp.innerHTML = "";
     loadGameScene();
   });
-  dayApp.append(yes);
+  container.appendChild(yes);
+  dayApp.append(container);
 
   // end game condition
-  if (money <= 0 || day >= 5) {
+  if (money <= 0 || day >= 10) {
     endGame();
   }
 }
@@ -341,7 +353,7 @@ function checkIfInvestmentSucceeded() {
     if (investment.time == 0) {
       const randRisk =
         Math.floor(
-          Math.random() * (risk_investment[1] - risk_investment[0] + 1),
+          Math.random() * (risk_investment[1] - risk_investment[0] + 1)
         ) + risk_investment[0];
       if (randRisk <= investment.risk) {
         money += investment.payout;
@@ -351,7 +363,7 @@ function checkIfInvestmentSucceeded() {
         pinSucess[investment.id] = 2;
       }
       const index = openInvestments.findIndex(
-        (item) => item.name === investment.name,
+        (item) => item.name === investment.name
       );
       if (index !== -1) openInvestments.splice(index, 1);
     }
@@ -365,7 +377,7 @@ function pinCreator(
   left: number,
   emoji: string,
   id: number,
-  isSuccess: number,
+  isSuccess: number
 ): HTMLElement {
   const text = document.createElement("span");
   text.textContent = emoji;
@@ -404,6 +416,15 @@ function pinCreator(
 function loadGameScene() {
   app.innerHTML = "";
 
+  const instructions = document.createElement("span");
+  instructions.style.display = "inline-block";
+  instructions.style.textAlign = "left";
+  instructions.innerText =
+    "Click on a pin to make an investment decision.\nSymbols matching the investments you supported will show on the board later on.\nGreen highlights show successful investments, and reds failures.";
+  instructions.style.position = "absolute";
+  instructions.style.top = `${height * 0.01}px`;
+  instructions.style.left = `${width * 0.01}px`;
+
   const img = document.createElement("img");
   img.src = "assets/map.png";
   img.style.position = "absolute";
@@ -413,6 +434,7 @@ function loadGameScene() {
   img.style.transform = "rotate(10deg)";
 
   app.appendChild(img);
+  app.appendChild(instructions);
   pinCreator(height * 0.67, width * 0.22, pinSymbols[1], 1, pinSucess[1]);
   pinCreator(height * 0.5, width * 0.7, pinSymbols[2], 2, pinSucess[2]);
   pinCreator(height * 0.9, width * 0.42, pinSymbols[3], 3, pinSucess[3]);
@@ -426,19 +448,14 @@ function endGame() {
   app.innerHTML = "";
   dialougeApp.innerHTML = "";
 
-  // img.style.transform = "rotate(10deg)";
-
   const closedInvestments: Investment[] = checkIfInvestmentSucceeded();
-  // const bg = document.createElement("div");
-  // bg.className = "new_day_rect"; // Defined in the CSS
-  // const container = document.createElement("span");
 
   let newPayments = "";
 
   openInvestments.forEach((investment) => {
     const randRisk =
       Math.floor(
-        Math.random() * (risk_investment[1] - risk_investment[0] + 1),
+        Math.random() * (risk_investment[1] - risk_investment[0] + 1)
       ) + risk_investment[0];
     if (randRisk <= investment.risk) {
       money += investment.payout;
@@ -448,46 +465,92 @@ function endGame() {
       pinSucess[investment.id] = 2;
     }
     const index = openInvestments.findIndex(
-      (item) => item.name === investment.name,
+      (item) => item.name === investment.name
     );
     if (index !== -1) openInvestments.splice(index, 1);
   });
 
   closedInvestments.forEach((investment) => {
-    newPayments += `${investment.name} got you ${investment.payout}`;
+    newPayments += `${investment.name} got you ${investment.payout}\n`;
   });
   // info about the game progress
   const img = document.createElement("img");
   img.src = "assets/end_letter1.png";
   img.style.position = "absolute";
-  img.style.top = `${height * 0.05}px`;
-  img.style.left = `${width * 0.05}px`;
-  img.style.width = `${width * 0.45}px`;
+  img.style.top = `${height * 0.01}px`;
+  img.style.left = `${width * 0.01}px`;
+  img.style.width = `${width * 0.41}px`;
   img.style.height = `${height * 0.7}px`;
   const text = document.createElement("span");
   text.style.display = "inline-block";
-  text.innerText = `Day: ${day}\nMoney:${money}\nAny payments:${newPayments}\n\nOut of ${day} days,\n\tyou chose the risker option ${numRiskyChoices} times\n\tand took the long-term reward ${numWaitChoices} times`;
+  text.innerText = `Day: ${day}\nMoney: ${money}\nAny payments: ${newPayments}\n\nOut of ${day} days,\n\tyou chose the risker option ${numRiskyChoices} times\n\tand took the long-term reward ${numWaitChoices} times`;
   text.style.position = "absolute";
-  text.style.top = `${height * 0.13}px`;
-  text.style.left = `${width * 0.12}px`;
+  text.style.top = `${height * 0.08}px`;
+  text.style.left = `${width * 0.07}px`;
 
   // info about the resources
   const img1 = document.createElement("img");
-  img1.src = "assets/end_letter1.png";
+  img1.src = "assets/end_letter2.png";
   img1.style.position = "absolute";
-  img1.style.top = `${height * 0.05}px`;
-  img1.style.left = `${width * 0.05}px`;
-  img1.style.width = `${width * 0.45}px`;
+  img1.style.top = `${height * 0.01}px`;
+  img1.style.left = `${width * 0.4}px`;
+  img1.style.width = `${width * 0.49}px`;
   img1.style.height = `${height * 0.7}px`;
   const text1 = document.createElement("span");
-  text1.style.display = "inline-block";
-  text1.innerText = `Day: ${day}\nMoney:${money}\nAny payments:${newPayments}\n\nOut of ${day} days,\n\tyou chose the risker option ${numRiskyChoices} times\n\tand took the long-term reward ${numWaitChoices} times`;
+  text1.className = "text-box";
+  text1.style.setProperty("--width", `${width * 0.35}px`);
+  text1.innerText = `Amir et al. (2020) was the biggest inspiration for this game, who found that across all countries, participants were more likely to choose the risky option as the stake size increased. However, countries that are not integrated in the marker were the least risk-seeking population. People are unsure when the next time they will receive food, so they prefer to stick with the safe option.
+                  \nKo et al. (2004) that Korean shoppers felt that it was more risky in regards to disapproval of the products by their friends and family, while American shoppers were worried about losing money, time. Korea is an interconnected country so their reputation is important to them, while America is a more individualistic country, so the American users will need to deal with the loss after a bad purchase by themselves. 
+  `;
   text1.style.position = "absolute";
-  text1.style.top = `${height * 0.13}px`;
-  text1.style.left = `${width * 0.12}px`;
+  text1.style.top = `${height * 0.08}px`;
+  text1.style.left = `${width * 0.46}px`;
+
+  const img3 = document.createElement("img");
+  img3.src = "assets/end_letter1.png";
+  img3.style.position = "absolute";
+  img3.style.top = `${height * 0.75}px`;
+  img3.style.left = `${width * 0.4}px`;
+  img3.style.width = `${width * 0.5}px`;
+  img3.style.height = `${height * 0.9}px`;
+  const text3 = document.createElement("span");
+  text3.className = "text-box";
+  text3.style.setProperty("--width", `${width * 0.35}px`);
+  text3.innerText = `Rieger et al. (2015) discovered that richer countries and those with a higher uncertainty avoidance index are risk averse in terms of gains but risk seeking in losses. Moreover, individualistic countries are less risk-averse in both gains and losses compared to collectivist countries. Again, people who are more financially secure do not fear being risk-seeking in terms of losses, and collectivistic countries have more support for their in-group members, so people can be more risk-seeking.
+                  \nAlmansour et al. (2023) found 5 factors that impact investment decisions: herding behviors (following others’ decisions), disposition (holding onto lost investments for long periods of time), blue chip stocks (bias towards well-established companies), overconfidence, and risk perception. 
+                  \nWang et al. (2017) found that individualism, socical status, masculinity, and uncertainty avoidance increase the degree of loss aversion across cultures. More specifically, Eastern European participants have the highest loss aversion on average, whereas African participants have the lowest.
+`;
+  text3.style.position = "absolute";
+  text3.style.top = `${height * 0.82}px`;
+  text3.style.left = `${width * 0.46}px`;
+
+  // info about the resources
+  const img2 = document.createElement("img");
+  img2.src = "assets/end_letter3.png";
+  img2.style.position = "absolute";
+  img2.style.top = `${height * 0.75}px`;
+  img2.style.left = `${width * 0.01}px`;
+  img2.style.width = `${width * 0.41}px`;
+  img2.style.height = `${height * 0.65}px`;
+  const text2 = document.createElement("span");
+  text2.className = "text-box";
+  text2.style.setProperty("--width", `${width * 0.25}px`);
+  text2.innerText = `Risk taking depends a lot on culture and risk perception.\nWhether your country has access to the market,\nwhether your country values individualism or collectivism,\nand whether your country is rich\ncan all impact the actions you will take as an individual.\n\nLook at the results that you got and look at the following passages to see if this simulation correctly aligned with your personal beliefs and the country you’re from.`;
+  text2.style.position = "absolute";
+  text2.style.top = `${height * 0.82}px`;
+  text2.style.left = `${width * 0.08}px`;
 
   dayApp.append(img);
   dayApp.append(text);
+
+  dayApp.append(img1);
+  dayApp.append(text1);
+
+  dayApp.append(img2);
+  dayApp.append(text2);
+
+  dayApp.append(img3);
+  dayApp.append(text3);
 }
 
 function startScreen() {
